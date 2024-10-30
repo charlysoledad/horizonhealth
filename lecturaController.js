5300c8e2b58a25561a766a933905f4c2bc80cad7
Juan Soledad <30356956+charlysoledad@users.noreply.github.com>
2024-10-29 20:51:35 -0600

    Update lecturaController.js

--- lecturaController.js
+++ lecturaController.js
@@ -20,6 +20,7 @@
      try {
          const [rows] = await db.query('SELECT * FROM lectura');
          res.json(rows);
+         res.status(200).json(rows)
      } catch (error) {
          console.error(error);
          res.status(500).json({ error: 'Error al obtener lecturas' });
